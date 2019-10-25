ccca5afc44f500a85fb859689ecf5e830ef8d8d3
x3TsunaYH <tyhshana@hotmail.com>
2019-10-25 23:58:05 +0800

    Set up webpage files

---
+++ base/mqtt_client/server/main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Hello extends React.Component {
+
+ render() {
+
+//JSX code in return function     
+
+  return (
+
+       <div>
+
+          <h1>Hello World!</h1>
+
+       </div>
+
+    );
+
+ }
+
+}
+
+render(<Hello/>, document.getElementById('app'));

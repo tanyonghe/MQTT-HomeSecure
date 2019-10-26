a2f2e22c24b8aa2ae2d8606cd761bff623481492
unknown <tanyonghe@u.nus.edu>
2019-10-27 01:58:05 +0800

    Clean npm files

---
+++ base/mqtt_client/localhost/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+app.get('/formSubmission', function (req, res) {
+  if (req.query.uname === 'admin' && req.query.psw === 'cs3103') {
+    res.redirect('dashboard.html');
+  } else {
+    res.redirect('index.html');
+  }
+});
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000!")
+});

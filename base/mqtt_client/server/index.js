50e529290b9b31f72e29acd1d9eb0f93f0037381
unknown <tanyonghe@u.nus.edu>
2019-10-26 23:19:07 +0800

    Update webpage UI

--- base/mqtt_client/server/index.js
+++ base/mqtt_client/server/index.js
@@ -7,6 +7,14 @@ app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
 
+app.get('/formSubmission', function (req, res) {
+  if (req.query.uname === 'admin' && req.query.psw === 'cs3103') {
+    res.redirect('dashboard.html');
+  } else {
+    res.redirect('index.html');
+  }
+});
+
 app.listen(3000, function(){
   console.log("Listening on port 3000!")
 });
